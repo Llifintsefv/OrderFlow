c80fffd33415103ed210abc8b12a7678a505ef16
lifintsefv <157622370+Llifintsefv@users.noreply.github.com>
2024-11-21 15:45:00 +0300

    fix db connect

--- services/order-service/cmd/main.go
+++ services/order-service/cmd/main.go
@@ -1,2 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+	
+)
+
+func main() {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+
+	// Подключение к базе данных
+	dbConn, err := db.NewDBConnection(dbHost, dbPort, dbUser, dbPass, dbName)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer dbConn.Close()
+}
